dea5bb14117bda20d78bc839cf6ee53c61d6c209
Emin Hamzagic <hamzagicemin4@gmail.com>
2021-10-26 20:05:45 +0200

    js

--- JavaScript/Cas1/main.js
+++
@@ -1,8 +0,0 @@
-a = 5;
-b = 6;
-
-p = a * b
-o = 2 * a + 2 * b
-
-console.log('Povrsina pravougaonika je: ', p)
-console.log('Obim pravougaonika je: ', o)
---
+++ JavaScript/Vezbanje/main.js
@@ -0,0 +1,30 @@
+// a = 5;
+// b = 6;
+
+// p = a * b
+// o = 2 * a + 2 * b
+
+// console.log('Povrsina pravougaonika je: ', p)
+// console.log('Obim pravougaonika je: ', o)
+
+
+// str = 'Hello'
+// console.log(str.concat(' ', 'World'))
+
+// function func1(){
+//     str = document.getElementById('h1').innerHTML
+//     document.getElementById('h1').innerHTML = str.replace('Hello', 'Hi')
+// }
+
+
+
+// let x = 5.32323232323
+
+// console.log(Math.sin(90))
+
+
+array = ['Hello', 'World', 3, true]
+
+for (let i = 0; i < array.length; i++){
+    console.log(array[i])
+}

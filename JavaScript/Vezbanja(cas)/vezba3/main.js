67684c6dd3c419293108ff11864d2f4e7c17e4af
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-01-31 21:26:42 +0100

    js

--- JavaScript/Vezbanja(cas)/vezba3/main.js
+++ JavaScript/Vezbanja(cas)/vezba3/main.js
@@ -1,8 +1,24 @@
-for (let i = 0; i < 20; i++){
-    let div = document.createElement('div');
-    div.classList.add('box');
-    document.body.appendChild(div);
-    div.addEventListener('click', () => {
-        div.classList.toggle('color');
-    })
+// for (let i = 0; i < 20; i++){
+//     let div = document.createElement('div');
+//     div.classList.add('box');
+//     document.body.appendChild(div);
+//     div.addEventListener('click', () => {
+//         div.classList.toggle('color');
+//     })
+// }
+
+
+var word = document.getElementById('lorem');
+var word_list = word.innerText.split(' ');
+var new_s = '';
+
+for (let el of word_list){
+    if (el.length > 10){
+        new_s += el.toUpperCase() + ' ';
+    }
+    else{
+        new_s += el + ' ';
+    }
 }
+
+word.innerText = new_s;

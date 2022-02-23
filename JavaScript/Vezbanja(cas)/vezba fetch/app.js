ebbcf223fe15e32c0f214a0f8b9ab3bfc4d40a20
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-02-23 21:27:20 +0100

    fetch vezba

--- JavaScript/Vezbanja(cas)/vezba fetch/app.js
+++ JavaScript/Vezbanja(cas)/vezba fetch/app.js
@@ -9,9 +9,22 @@ getData().then((data) => {
     console.log(data);
     var container = document.createElement('div');
     container.classList.add('container');
-    var image = document.createElement('img').classList.add('img');
+    var image = document.createElement('img');
+    image.classList.add('img')
     image.src = data.avatar_url;
-    container.appendChild(image)
+    container.appendChild(image);
+    username_div = document.createElement('div');
+    username_div.classList.add('username_div');
+    username = document.createElement('h1');
+    username.innerText = data.name;
+    loginname = document.createElement('p');
+    loginname.innerText = data.login;
+
+    username_div.appendChild(username);
+    username_div.appendChild(loginname);
+    container.appendChild(username_div);
+
+    
 
     document.body.appendChild(container);
 })

141e9781cfdd82d52349249efedf20e4a6ddd9e1
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-02-24 19:38:43 +0100

    fetch vezba finished

--- JavaScript/Vezbanja(cas)/vezba fetch/app.js
+++ JavaScript/Vezbanja(cas)/vezba fetch/app.js
@@ -1,12 +1,19 @@
-async function getData(){
-    var respone = await fetch('https://api.github.com/users/EminHamzagic');
+const canvas = document.querySelector('.canvas');
+
+async function getUserData(user){
+    var respone = await fetch(`https://api.github.com/users/${user}`);
     const data = await respone.json();
     return data
 }
 
-getData().then((data) => {
-    const profileData = data;
-    console.log(data);
+async function getUserList(link){
+    var respone = await fetch(link);
+    const data = await respone.json();
+    return data
+}
+
+function profileCard(data){
+    // const profileData = data;
     var container = document.createElement('div');
     container.classList.add('container');
     var image = document.createElement('img');
@@ -24,7 +31,83 @@ getData().then((data) => {
     username_div.appendChild(loginname);
     container.appendChild(username_div);
 
+    followInfoDiv = document.createElement('div');
+    followInfoDiv.classList.add('followDiv');
+    followInfoDiv.innerHTML += `<p class="getList"><i class="fa-solid fa-user-group"></i> ${data.followers} followers</p>`;
+    followInfoDiv.innerHTML += `<p class="getList"><i class="fa-solid fa-user-group"></i> ${data.following} following</p>`;
+    container.appendChild(followInfoDiv);
     
+    repos = document.createElement('p');
+    repos.innerText = `${data.public_repos} repositories`;
+    repos.style.fontSize = '18px';
+    repos.style.width = '85%';
+    repos.style.height = '25%';
+    repos.style.color = 'rgb(37, 37, 37)';
+    repos.classList.add('repos');
+    container.appendChild(repos);
+    
+    createDate = document.createElement('p');
+    createDate.style.color = 'rgb(37, 37, 37)';
+    createDate.style.width = '85%';
+    createDate.innerText = `created at ${data.created_at.slice(0, 9)}`;
+    
+    container.appendChild(createDate);
+    
+    canvas.appendChild(container);
 
-    document.body.appendChild(container);
+    const followLists = document.getElementsByClassName('getList');
+    for (let list of followLists){
+        list.addEventListener('click', (e) => {
+            if (e.target.innerText.includes('followers')){
+                getUserList(data.followers_url).then((data) => {
+                    canvas.innerHTML = '';
+                    userList(data);
+                })
+            }
+            else {
+                getUserList(`https://api.github.com/users/${data.login}/following`).then((data) => {
+                    canvas.innerHTML = '';
+                    userList(data);
+                })
+            }
+        });
+    }
+}
+
+function userList(data){
+    var container = document.createElement('div');
+    container.classList.add('listContainer');
+
+    for(let user of data){
+        let userDiv = document.createElement('div');
+        userDiv.classList.add('user_div');
+
+        userAvatar = document.createElement('img');
+        userAvatar.src = user.avatar_url;
+        userDiv.appendChild(userAvatar);
+
+        username = document.createElement('p');
+        username.classList.add('user')
+        username.innerText = user.login;
+        username.style.color = 'rgb(37, 37, 37)';
+        username.style.cursor = 'pointer';
+        userDiv.appendChild(username)
+
+        container.appendChild(userDiv);
+    }
+    
+    canvas.appendChild(container);
+    const usersList = document.getElementsByClassName('user');
+    for (let user of usersList){
+        user.addEventListener('click', () => {
+            getUserData(user.innerText).then((data) => {
+                canvas.innerHTML = '';
+                profileCard(data);
+            })
+        })
+    }
+}
+
+getUserData('EminHamzagic').then((data) => {
+    profileCard(data);
 })
